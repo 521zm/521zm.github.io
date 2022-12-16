5c853e8d55d4abb8c85b78b45c161a3f46818131
521zm <97543110+521zm@users.noreply.github.com>
2022-12-16 10:46:42 +0800

    change

--- recard-screen/main.js
+++ recard-screen/main.js
@@ -23,9 +23,11 @@ $(function () {
     if (flag) {
       flag = false;
       $(this).css('backgroundColor', '#eff0f1');
+      personViewFunStop()
     } else {
       flag = true;
       $(this).css('backgroundColor', '#e6ffd2');
+      personViewFun()
     }
   });
   $('#audio').click(function () {
@@ -49,11 +51,24 @@ $(function () {
   const video = document.querySelector('#video')
   const mask_three = document.querySelector('.mask-three')
   const three = document.querySelector('.three')
-  startRecard.addEventListener('click', async function () {
-    let stream = await navigator.mediaDevices.getDisplayMedia({
+  const stopRecard = document.querySelector('.stop')
+  let mediaRecorder
+  startRecard.addEventListener('click', async () => {
+    // 获取视频流
+    let videoStream = await navigator.mediaDevices.getDisplayMedia({
       video: true,
+      audio: true
     })
-    let mediaRecorder = new MediaRecorder(stream);
+    // 获取音频流
+    let audioStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    })
+    // 将视频流与音频流合并实例化新的媒体流
+    let tracks = []
+    videoStream.getVideoTracks().forEach(t => tracks.push(t))
+    audioStream.getAudioTracks().forEach(t => tracks.push(t))
+    let stream = new MediaStream(tracks)
+    mediaRecorder = new MediaRecorder(stream);
     //三秒后启动
     setTimeout(() => {
       mediaRecorder.start()
@@ -61,14 +76,16 @@ $(function () {
     // 蒙版
     mask_three.className = 'mask-three-block'
     // three三秒倒计时
-    let t = 2;
+    let t = 3;
+    three.innerHTML = t;
     let timer = setInterval(function () {
-      three.innerHTML = t;
       t--;
-      if (t < 0) {
+      if (t <= 0) {
         clearInterval(timer);
       }
+      three.innerHTML = t;
     }, 1000)
+
     // 三秒后清空蒙版
     setTimeout(() => {
       mask_three.className = 'mask-three'
@@ -78,8 +95,9 @@ $(function () {
     mediaRecorder.addEventListener('dataavailable', function (e) {
       chunks.push(e.data)
     });
-    //停止
+    //停止录制视频
     mediaRecorder.addEventListener('stop', function () {
+      this.stream.getTracks().forEach(track => track.stop())
       let blob = new Blob(chunks, {
         type: chunks[0].type,
       });
@@ -92,7 +110,7 @@ $(function () {
         .replace(' ', '_')
         .replace(/:/g, '-')
       a.href = url
-      a.download = `${name}.webm`
+      a.download = `${name}.mp4`
       video.src = url
       video.className = 'view'
       a.click()
@@ -100,7 +118,57 @@ $(function () {
 
     })
   });
+  // 停止录屏(音频与视频)
+  stopRecard.addEventListener('click', function () {
+    mediaRecorder.stop()
+  })
   document.body.addEventListener('click', function () {
     video.className = ''
   })
+  const clickPerson = document.querySelector('.select-container').querySelector('#person')
+  const person_screen = document.querySelector('#person-screen')
+  const screen = document.querySelector('#screen')
+  const personView = document.querySelector('#personView')
+  // 调用摄像头的方法
+  let mediaRecorder1
+  const personViewFun = async function () {
+    let personStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    })
+    mediaRecorder1 = new MediaRecorder(personStream);
+    personView.className = 'person'
+    personView.srcObject = personStream
+    mediaRecorder1.start()
+    // let personData = []
+    // mediaRecorder1.addEventListener('dataavailable', function (e) {
+    //   personData.push(e.data)
+    // });
+    //停止录制人像
+    mediaRecorder1.addEventListener('stop', function () {
+      this.stream.getTracks().forEach(track => track.stop())
+      // let blob = new Blob(personData, {
+      //   type: personData[0].type,
+      // });
+      // let url = URL.createObjectURL(blob);
+      // console.log(personView.src);
+      // personView.src = url
+    })
+  }
+  // 取消摄像头
+  const personViewFunStop = function () {
+    mediaRecorder1.stop()
+    personView.className = 'person-none'
+  }
+  // personViewFun()
+  clickPerson.addEventListener('click', () => {
+    personViewFun()
+  })
+  person_screen.addEventListener('click', () => {
+    personViewFun()
+  })
+  screen.addEventListener('click', () => {
+    personViewFunStop()
+  })
+  // 摄像头默认打开
+  clickPerson.click()
 });
